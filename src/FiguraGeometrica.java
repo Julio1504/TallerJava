42ecb799dfc59ccd49284be2ee842024e1614045
Julio <julianorozcol1504@gmail.com>
2024-05-07 08:16:45 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por [Julian Orozco]

--- src/FiguraGeometrica.java
+++ src/FiguraGeometrica.java
@@ -1,4 +1,25 @@
 public class FiguraGeometrica {
     private String nombre;
     private String colorFigura;
+
+    public FiguraGeometrica(String nombre, String colorFigura) {
+        this.nombre = nombre;
+        this.colorFigura = colorFigura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColorFigura() {
+        return colorFigura;
+    }
+
+    public void setColorFigura(String colorFigura) {
+        this.colorFigura = colorFigura;
+    }
 }
