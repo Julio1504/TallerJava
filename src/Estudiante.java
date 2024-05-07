fb965c1d71fe45d2ad7ae48e2def2e798411efd6
Julio <julianorozcol1504@gmail.com>
2024-05-07 07:52:21 -0500

    Herencia - Implementación de Constructores por [Julian Orozco]

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,6 +1,11 @@
 public class Estudiante extends Persona{
     private String grado;
 
+    public Estudiante(String nombre, Integer edad, String fechaNacimiento, String grado) {
+        super(nombre, edad, fechaNacimiento);
+        this.grado = grado;
+    }
+
     public String getGrado() {
         return grado;
     }
