3fba2f1ceba7ddf822f48734ba34e516e6d867cd
Julio <julianorozcol1504@gmail.com>
2024-05-07 08:06:06 -0500

    Polimorfismo - Implementación en Clase Empleado por [Julian Orozco]

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,5 +1,8 @@
-public class Empleado extends Persona {
+public class Empleado extends Persona implements Trabajador{
     public Empleado(String nombre, Integer edad, String fechaNacimiento) {
         super(nombre, edad, fechaNacimiento);
+    }
+    public void realizarTarea(){
+        System.out.println("Realizando tarea laboral");
     }
 }
