00ca37e34449ad2b6a00139a63ba5bdf1e67ddfc
Julio <julianorozcol1504@gmail.com>
2024-05-07 07:48:28 -0500

    Herencia - Implementación de Propiedad adicional para el grado por [Julian Orozco]

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,3 +1,11 @@
 public class Estudiante extends Persona{
+    private String grado;
 
+    public String getGrado() {
+        return grado;
+    }
+
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
 }
