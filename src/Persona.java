db8f1b6cb906ee387d05ddac2a84c286a4f91050
Julio <julianorozcol1504@gmail.com>
2024-05-07 07:29:34 -0500

    Encapsulamiento - Implementación de Validaciones por [Julian Orozco]

--- src/Persona.java
+++ src/Persona.java
@@ -22,7 +22,13 @@ public class Persona {
     }
 
     public void setEdad(Integer edad) {
-        this.edad = edad;
+        if (edad > 0 ){
+            this.edad = edad;
+            System.out.println("Edad establecida");
+        }
+        else{
+            System.out.println("Ingrese una edad valida");
+        }
     }
 
     public String getFechaNacimiento() {
